a228a31c491afcf94bb1f38d5142d3a1ca9cd1df
dansab <dan.sabadas@gmail.com>
2016-07-25 16:00:51 +0300

    saved some data binding stuff

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html'
-    /*template: `
-        <h1>{{title}}</h1>
-        <div [style.color]="wardsColor">
-        Hello {{name}}
-        </div>
-        <button (click)="changeSuitColor()"></button>
-    `*/
 })
 export class AppComponent { 
     title = 'Customer App';
@@ -20,4 +13,12 @@ export class AppComponent {
     {
         this.wardsColor = this.wardsColor == 'red' ? 'blue' : 'red';
     }
+
+        /*template: `
+        <h1>{{title}}</h1>
+        <div [style.color]="wardsColor">
+        Hello {{name}}
+        </div>
+        <button (click)="changeSuitColor()"></button>
+    `*/
 }
